0f628d4e4d1940dfbfa4f3a276939a3b89a0f05c
Andrew Hammond <andrew@evertrue.com>
2013-08-20 09:31:59 -0400

    Fix formatting

--- src/HelpScout/model/Attachment.php
+++ src/HelpScout/model/Attachment.php
@@ -1,20 +1,22 @@
 <?php
 namespace HelpScout\model;
 
-class Attachment {
-	private $id;
-	private $mimeType;
-	private $fileName;
-	private $size   = 0;
-	private $width  = 0;
-	private $height = 0;
-	private $url;
+class Attachment
+{
+    private $id;
+    private $mimeType;
+    private $fileName;
+    private $size   = 0;
+    private $width  = 0;
+    private $height = 0;
+    private $url;
     private $hash;
 
-	private $data;
+    private $data;
 
-	public function __construct($data=null) {
-		if ($data) {
+    public function __construct ($data = null)
+    {
+        if ($data) {
             $this->id       = isset($data->id)       ? $data->id       : null;
             $this->mimeType = isset($data->mimeType) ? $data->mimeType : null;
             $this->fileName = isset($data->fileName) ? $data->fileName : null;
@@ -23,131 +25,146 @@ class Attachment {
             $this->height   = isset($data->height)   ? $data->height   : null;
             $this->url      = isset($data->url)      ? $data->url      : null;
             $this->hash     = isset($data->hash)     ? $data->hash     : null;
-		}
-	}
+        }
+    }
 
-	public function load($filename) {
-		if (file_exists($filename)) {
-			$this->data     = file_get_contents($filename);
-			$this->fileName = basename($filename);
-			$this->mimeType = mime_content_type($filename);
-			$this->size     = filesize($filename);
+    public function load($filename)
+    {
+        if (file_exists($filename)) {
+            $this->data     = file_get_contents($filename);
+            $this->fileName = basename($filename);
+            $this->mimeType = mime_content_type($filename);
+            $this->size     = filesize($filename);
 
-			if (substr($this->mimeType, 0, 5) == 'image') {
-				$width  = 0;
-				$height = 0;
+            if (substr($this->mimeType, 0, 5) == 'image') {
+                $width  = 0;
+                $height = 0;
 
-				$info = getimagesize($filename);
-				if ($info) {
-					if (isset($info[0])) {
-						$width = $info[0];
-					}
-					if (isset($info[1])) {
-						$height = $info[1];
-					}
-				}
-				$this->width  = $width;
-				$this->height = $height;
-			}
-		}
-	}
-    public function getObjectVars() {
+                $info = getimagesize($filename);
+                if ($info) {
+                    if (isset($info[0])) {
+                        $width = $info[0];
+                    }
+                    if (isset($info[1])) {
+                        $height = $info[1];
+                    }
+                }
+                $this->width  = $width;
+                $this->height = $height;
+            }
+        }
+    }
+    public function getObjectVars()
+    {
         return get_object_vars($this);
     }
 
-    public function toJson() {
-    	$vars = get_object_vars($this);
-    	$vars['data'] = base64_encode($this->data);
+    public function toJson()
+    {
+        $vars = get_object_vars($this);
+        $vars['data'] = base64_encode($this->data);
 
-  		return json_encode($vars);
+        return json_encode($vars);
     }
 
     /**
      * @param $data
      */
-    public function setData($data) {
+    public function setData($data)
+    {
         $this->data = $data;
     }
 
-	/**
-	 * Returns the raw data for this attachment.
-	 *
-	 * @return string
-	 */
-	public function getData() {
-		if ($this->data === false) {
-			$this->data = \HelpScout\ApiClient::getInstance()->getAttachmentData($this->id);
-		}
-		return $this->data;
-	}
+    /**
+     * Returns the raw data for this attachment.
+     *
+     * @return string
+     */
+    public function getData()
+    {
+        if ($this->data === false) {
+            $this->data = \HelpScout\ApiClient::getInstance()->getAttachmentData($this->id);
+        }
+        return $this->data;
+    }
 
     /**
      * @param $fileName
      */
-    public function setFileName($fileName) {
+    public function setFileName($fileName)
+    {
         $this->fileName = $fileName;
     }
 
     /**
      * @param $mimeType
      */
-    public function setMimeType($mimeType) {
+    public function setMimeType($mimeType)
+    {
         $this->mimeType = $mimeType;
     }
 
     /**
      * @param $hash
      */
-    public function setHash($hash) {
+    public function setHash($hash)
+    {
         $this->hash = $hash;
     }
 
-	/**
-	 * @return int
-	 */
-	public function getId() {
-		return $this->id;
-	}
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getMimeType() {
-		return $this->mimeType;
-	}
+    /**
+     * @return string
+     */
+    public function getMimeType()
+    {
+        return $this->mimeType;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getFileName() {
-		return $this->fileName;
-	}
+    /**
+     * @return string
+     */
+    public function getFileName()
+    {
+        return $this->fileName;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getSize() {
-		return $this->size;
-	}
+    /**
+     * @return int
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getWidth() {
-		return $this->width;
-	}
+    /**
+     * @return int
+     */
+    public function getWidth()
+    {
+        return $this->width;
+    }
 
-	/**
-	 * @return int
-	 */
-	public function getHeight() {
-		return $this->height;
-	}
+    /**
+     * @return int
+     */
+    public function getHeight()
+    {
+        return $this->height;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getUrl() {
-		return $this->url;
-	}
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
 }
